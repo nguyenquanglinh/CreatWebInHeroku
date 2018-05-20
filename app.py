3f4d9114f878ed4a2ff386d5e26dec5910a8dd0f
quang <thangnguyen2000x99nd@gmail.com>
2018-05-20 22:27:59 +0700

    v1

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import flask
+
 app = flask.Flask(__name__)
+
+
 @app.route("/")
 def homepage():
-    return "Hello world!"
+    return "Hello world!1111 ww"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
